f83cdfb456a98e4a682ba6bbdb0170ed15faf07b
fauzaanirsyaadi <fauzaan.irsyaadi@gmail.com>
2021-10-21 15:40:45 +0700

    [add] initial

---
+++ PaymentAPIMysql/Models/PaymentDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace PaymentAPIMysql.Models
+{
+    public class PaymentDetail
+    {
+        private Models.PaymentDetailContext context;
+
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; }
+    }
+}
